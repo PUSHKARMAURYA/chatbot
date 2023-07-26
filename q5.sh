38707a5aae5cb9909e4e3c80558d524240207596
PUSHKARMAURYA <60316721+PUSHKARMAURYA@users.noreply.github.com>
2023-07-26 13:50:59 +0530

    Update q5.sh

--- q5.sh
+++ q5.sh
@@ -1,41 +1,16 @@
 #!/bin/bash
 
-display_chessboard() {
-    local -r board_size=8
-    local -r tile_size=5
-
-    declare -A chessboard
-
-    # Set up the chessboard with alternating colors
-    for ((row=0; row<$board_size; row++)); do
-        for ((col=0; col<$board_size; col++)); do
+print_chessboard() {
+    for row in {1..8}; do
+        for col in {1..8}; do
             if (( (row + col) % 2 == 0 )); then
-                chessboard[$row,$col]="\e[47m"
+                echo -n "█ "  # Black square
             else
-                chessboard[$row,$col]="\e[100m"
+                echo -n "  "  # White square
             fi
         done
+        echo
     done
+}
 
-    # Add chess pieces
-    chessboard[0,0]="\e[91m♜\e[0m"
-    chessboard[0,1]="\e[91m♞\e[0m"
-    chessboard[0,2]="\e[91m♝\e[0m"
-    chessboard[0,3]="\e[91m♛\e[0m"
-    chessboard[0,4]="\e[91m♚\e[0m"
-    chessboard[0,5]="\e[91m♝\e[0m"
-    chessboard[0,6]="\e[91m♞\e[0m"
-    chessboard[0,7]="\e[91m♜\e[0m"
-    chessboard[1,0]="\e[91m♟\e[0m"
-    chessboard[1,1]="\e[91m♟\e[0m"
-    chessboard[1,2]="\e[91m♟\e[0m"
-    chessboard[1,3]="\e[91m♟\e[0m"
-    chessboard[1,4]="\e[91m♟\e[0m"
-    chessboard[1,5]="\e[91m♟\e[0m"
-    chessboard[1,6]="\e[91m♟\e[0m"
-    chessboard[1,7]="\e[91m♟\e[0m"
-    chessboard[7,0]="\e[96m♖\e[0m"
-    chessboard[7,1]="\e[96m♘\e[0m"
-    chessboard[7,2]="\e[96m♗\e[0m"
-    chessboard[7,3]="\e[96m♕\e[0m"
-    chessboard[
+print_chessboard
