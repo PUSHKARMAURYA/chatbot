0ebbca85b221468b216609f04aa9bcf567fb42b1
PUSHKARMAURYA <60316721+PUSHKARMAURYA@users.noreply.github.com>
2023-07-26 23:50:49 +0530

    Update q5.sh

--- q5.sh
+++ q5.sh
@@ -1,30 +1,30 @@
 
-  #Prompt the user to enter the name of a directory
-  read -p "Enter directory name: "dir
+    # Prompt the user to enter the name of a directory
+read -p "Enter directory name: " dir
 
-     #Check if the directory exists
-  if [! -d "$dir"]; then
+   # Check if the directory exists
+if [ ! -d "$dir" ]; then
     echo "Error: Directory '$dir' does not exist."
     exit 1
-  fi
+fi
 
-      #List all the files in the given directory
+    #  List all the files in the given directory
 echo "Files in '$dir':"
-  files=$(ls "$dir")
- echo "$files"
+files=$(ls "$dir")
+echo "$files"
 
   # Sort the files alphabetically
-   sorted=$(echo "$files" |sort)
+sorted=$(echo "$files" | sort)
 
-  # Create a new directory named "sorted" inside the given directory
-  sorted_dir="$dir/sorted"
-  mkdir -p "$sorted_dir"
+ # Create a new directory named "sorted" inside the given directory
+sorted_dir="$dir/sorted"
+mkdir -p "$sorted_dir"
 
-#Move each file from the original directory to the "sorted" directory
-  for file in $sorted; do
+    # Move each file from the original directory to the "sorted" directory
+for file in $sorted; do
     mv "$dir/$file" "$sorted_dir/"
-  done
+done
 
-  #Display a success message with the total number of files moved
- num_files=$(echo "$sorted" |wc -w)
-   echo "Successfully moved $num_files files to '$sorted_dir'."
+ # Display a success message with the total number of files moved
+num_files=$(echo "$sorted" | wc -w)
+echo "Successfully moved $num_files files to '$sorted_dir'."
