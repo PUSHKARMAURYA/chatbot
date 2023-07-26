d9a7a83ce6acc47d467a0c6a16cb047f2c0d3064
PUSHKARMAURYA <60316721+PUSHKARMAURYA@users.noreply.github.com>
2023-07-26 23:54:04 +0530

    Update q5.sh

--- q5.sh
+++ q5.sh
@@ -1,30 +1,30 @@
 
-    # Prompt the user to enter the name of a directory
+    # Ask user to enter the name of a folder
 read -p "Enter directory name: " dir
 
    # Check if the directory exists
 if [ ! -d "$dir" ]; then
-    echo "Error: Directory '$dir' does not exist."
+    echo "Error: folder '$dir' does not exist."
     exit 1
 fi
 
-    #  List all the files in the given directory
+    #  List all the files in the given direct
 echo "Files in '$dir':"
 files=$(ls "$dir")
 echo "$files"
 
-  # Sort the files alphabetically
+  # Sort the files
 sorted=$(echo "$files" | sort)
 
- # Create a new directory named "sorted" inside the given directory
+ # Create a new directory named "sorted" 
 sorted_dir="$dir/sorted"
 mkdir -p "$sorted_dir"
 
-    # Move each file from the original directory to the "sorted" directory
+    # Move each file from the original directory to the "sorted"
 for file in $sorted; do
     mv "$dir/$file" "$sorted_dir/"
 done
 
- # Display a success message with the total number of files moved
+ # Display a success messg
 num_files=$(echo "$sorted" | wc -w)
 echo "Successfully moved $num_files files to '$sorted_dir'."
